7ec75c4ea5e0400a364989a3cdafdb5b0a425cb1
Digislaw <digi2978@gmail.com>
2020-03-29 17:26:13 +0200

    Optymalniejszy patrol przeciwnika

--- Assets/Scripts/EnemyPatrol.cs
+++ Assets/Scripts/EnemyPatrol.cs
@@ -12,86 +12,6 @@ public class EnemyPatrol : MonoBehaviour
     private Animator anim;
 
     private bool facingLeft = true;
-
-    private float moveCounter;
-    [SerializeField] private float baseMoveTime;
-
-    private float waitCounter;
-    [SerializeField] private float baseWaitTime;
-
-    private void Awake()
-    {
-        rb = GetComponent<Rigidbody2D>();
-        sr = GetComponent<SpriteRenderer>();
-        anim = GetComponent<Animator>();
-
-        moveCounter = Random.Range(baseMoveTime * 0.5f, baseMoveTime * 1.5f);
-    }
-
-    private void FixedUpdate()
-    {
-        Debug.Log(moveCounter);
-        if (moveCounter > 0f)
-        {
-            anim.SetBool("Moving", true);
-            Move();
-            moveCounter -= Time.fixedDeltaTime;
-
-            if (moveCounter <= 0)
-                waitCounter = Random.Range(baseWaitTime * 0.5f, baseWaitTime * 1.5f);
-        }
-        else
-        {
-            anim.SetBool("Moving", false);
-            rb.velocity = new Vector2(0f, rb.velocity.y);
-            waitCounter -= Time.fixedDeltaTime;
-
-            if (waitCounter <= 0)
-                moveCounter = Random.Range(baseMoveTime * 0.5f, baseMoveTime * 1.5f);
-        }
-
-        //anim.SetBool("Moving", true);
-    }
-
-    private void Move()
-    {
-        if (facingLeft)
-        {
-            rb.velocity = new Vector2(-speed, rb.velocity.y);
-
-            if (transform.position.x < leftPoint.position.x)
-                Flip();
-        }
-        else
-        {
-            rb.velocity = new Vector2(speed, rb.velocity.y);
-
-            if (transform.position.x > rightPoint.position.x)
-                Flip();
-        }
-    }
-
-    private void Flip()
-    {
-        facingLeft = !facingLeft;
-        sr.flipX = !facingLeft;
-    }
-}
-
-
-/*
-public class EnemyPatrol : MonoBehaviour
-{
-    [Header("Configuration")]
-    [SerializeField] private float speed;
-    [SerializeField] private Transform leftPoint;
-    [SerializeField] private Transform rightPoint;
-
-    private Rigidbody2D rb;
-    private SpriteRenderer sr;
-    private Animator anim;
-
-    private bool facingLeft = true;
     private bool moving = true;
 
     private float counter;
@@ -161,4 +81,3 @@ public class EnemyPatrol : MonoBehaviour
         sr.flipX = !facingLeft;
     }
 }
-*/
