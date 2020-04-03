2f368708a913b3f25ad433e4915e0e0448243428
Digislaw <digi2978@gmail.com>
2020-04-03 20:07:37 +0200

    Menu pauzy

---
+++ Assets/Scripts/UI/PauseController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PauseController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject pauseMenu;   // obiekt nadrzedny menu pauzy
+    [SerializeField]
+    private AudioClip pauseSound;   // dzwiek wlaczenia/wylaczenia pauzy
+    private bool paused = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            SwitchPause();
+    }
+
+    public void SwitchPause()
+    {
+        paused = !paused;
+
+        AudioController.Instance.Play(pauseSound);
+        pauseMenu.SetActive(paused);
+        Time.timeScale = paused ? 0f : 1f;
+    }
+}
