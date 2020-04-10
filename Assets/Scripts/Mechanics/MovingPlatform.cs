dde7441efe6c85e265ff09978ade994acc052b4f
Digislaw <digi2978@gmail.com>
2020-04-10 19:27:45 +0200

    Ruchome platformy 2 - na bazie fizyki

--- Assets/Scripts/Mechanics/MovingPlatform.cs
+++ Assets/Scripts/Mechanics/MovingPlatform.cs
@@ -4,15 +4,22 @@ public class MovingPlatform : MonoBehaviour
 {
     [SerializeField]
     private Transform[] waypoints;
-    [SerializeField] [Range(0.1f, 2.5f)]
+    [SerializeField] [Range(0.1f, 5f)]
     private float speed = 2f;
     private int index = 0;  // indeks aktualnego punktu
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     private void FixedUpdate()
     {
-        transform.position = Vector3.MoveTowards(transform.position, waypoints[index].position, speed * Time.deltaTime);
+        //transform.position = Vector3.MoveTowards(transform.position, waypoints[index].position, speed * Time.deltaTime);
+        rb.MovePosition(Vector3.MoveTowards(rb.position, waypoints[index].position, speed * Time.fixedDeltaTime));
 
-        if (Vector3.Distance(transform.position, waypoints[index].position) < 0.1f)
+        if (Vector3.Distance(rb.position, waypoints[index].position) < 0.1f)
             index = (index + 1) % waypoints.Length;
     }
 
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -37,8 +37,6 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private AudioClip knockbackSound;
     [SerializeField] private AudioClip jumpSound;
 
-    private LayerMask platforms;
-
     private void Awake()
     {
         _instance = this;
@@ -46,8 +44,6 @@ public class PlayerMovement : MonoBehaviour
         sprite = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-
-        platforms = LayerMask.NameToLayer("Platforms");
     }
 
     private void Update()
@@ -148,17 +144,5 @@ public class PlayerMovement : MonoBehaviour
     public void Bounce()
     {
         rb.velocity = new Vector2(0.5f * bounceForce * direction, bounceForce);
-    }
-
-    private void OnCollisionEnter2D(Collision2D col)
-    {
-        if (col.gameObject.layer == platforms)
-            transform.parent = col.transform;
-    }
-
-    private void OnCollisionExit2D(Collision2D col)
-    {
-        if (col.gameObject.layer == platforms)
-            transform.parent = null;
     }
 }
