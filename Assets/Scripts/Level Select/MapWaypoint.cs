a77d993e0c2b41e92e298b5746e26536632a101f
Digislaw <digi2978@gmail.com>
2020-07-09 12:35:10 +0200

    Fundament wybierania poziomów - mapa, punkty

---
+++ Assets/Scripts/Level Select/MapWaypoint.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapWaypoint : MonoBehaviour
+{
+    [SerializeField]
+    private bool levelPoint;    // punkt reprezentuje poziom
+
+    [SerializeField]
+    private MapWaypoint north, west, east, south;
+}
