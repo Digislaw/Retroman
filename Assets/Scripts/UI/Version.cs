03e990d8aaeb906bea115fca2fac3d13c4c25d65
Digislaw <digi2978@gmail.com>
2020-07-24 13:50:27 +0200

    Menu główne

---
+++ Assets/Scripts/UI/Version.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Version : MonoBehaviour
+{
+    private Text version;
+
+    private void Awake()
+    {
+        version = GetComponent<Text>();
+        version.text = "v" + Application.version;
+    }
+}
